5a64ff43eb76fadd5f33934fb09f10044790d2bc
sampath1243 <puttapakasampath4@gmail.com>
2026-05-31 14:15:20 +0530

    updated files

---
+++ javascript/day1/script.js
@@ -0,0 +1,17 @@
+// console.log("Hello");
+// var sam=10;
+// var path=5;
+// console.log(sam+path);
+// if (sam<path){
+//     console.log((path)+" IS GREATER THAN "+(sam))
+// }else if(sam===path){
+//     console.log(sam+" and "+path+" both are equal values")
+// }else{
+//     console.log(path+" is less than "+sam)
+// }
+
+var num1=10;
+var num2=20;
+console.log(num1*num2);
+console.log(num2/num1);
+console.log(num1/num2);
---
+++ javascript/day2/script.js
@@ -0,0 +1,23 @@
+
+// console.log(document.body);
+
+//let para=document.getElementsByTagName("p")
+//console.log(para[2]);
+
+//let heading=document.getElementsByTagName("h1")[0]
+
+//console.log(heading);
+
+// let buchi=document.getElementsByClassName("buchi")
+// console.log(buchi[0]);
+
+// let boo=document.getElementById("boo")
+
+// //boo.innerText = "hello"
+// //boo.innerText = "<u>hello</u>"
+// boo.innerHTML= "<u>hello</u>"
+
+let paras = document.getElementsByTagName("p")
+
+paras[4].innerHTML="hello"
+document.getElementsByClassName("para")[1].innerHTML="welcome"
